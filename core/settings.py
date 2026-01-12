bc99cd7cfd250872ee041c2c75474349dd49c191
Naaz-Asim <naaz.infomover@gmail.com>
2026-01-12 13:33:54 +0530

    Initial Django project setup for WPMS

---
+++ core/settings.py
@@ -0,0 +1,33 @@
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # third party
+    "rest_framework",
+
+    # local apps
+    "accounts",
+    "employees",
+    "attendance",
+    "leaves",
+    "payroll",
+    "reports",
+]
+
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
